b30c6b6cb1c32100d5d790895cb9181750726f85
Keistie <mh5448@nyu.edu>
2022-09-04 00:42:19 -0400

    some change (trying figure out collision)

---
+++ Assets/Collision.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float distance;
+
+    public GameObject player;
+
+    Vector2 movement;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.y = distance;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Car"){
+
+            Debug.Log("Hit");
+            //this.gameObject.SetActive(false);
+            player.GetComponent<CHMovement>().enabled = false;
+            rb.MovePosition(rb.position - movement * Time.fixedDeltaTime);
+            player.GetComponent<CHMovement>().enabled = true;
+        }
+       // player.GetComponent<CHMovement>().enabled = true;
+        //this.gameObject.SetActive(true);
+    }
+}
